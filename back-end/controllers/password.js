9eb6d1905ccf7c9d4f7d551b61433a12feeb43eb
Koffi <winston.aguemon@gmail.com>
2021-11-05 13:23:28 +0000

    changement message erreur password

--- back-end/controllers/password.js
+++ back-end/controllers/password.js
@@ -2,7 +2,7 @@ const passwordSchema = require('../models/password');
 
  module.exports = (req, res, next) => {
      if (!passwordSchema.validate(req.body.password)) {
-         res.status(400).json({ message: 'Le MDP doit faire 10 caractère au moins, avec une maj, une min et un chiffre au moins.' });
+         res.status(400).json({ message: 'Le mot de passe doit faire 10 caractère minimum, avec une majuscule, une minusculte et un chiffre.' });
      } else {
          next();     }
 };
